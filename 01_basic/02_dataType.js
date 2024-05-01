72d86ddc9acedcfdf8a6c65f82800a6a1650ad05
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-05-01 20:15:36 +0000

    dataType in js

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3+3); // we are using nodejs, not browser.
+
+console.log(3
+    +           // code readability shou;d be high
+        3)
+console.log("Amit")
+
+// link of javaScript documentation : https://tc39.es/ecma262/
+
+// or link mdn
+
+let name = "Amit"
+let age =21
+let isLoggedIN = false
+
+// Data Typr in javascript are:
+// number => in range ( 2 to power 53)
+// bigint
+// string = > " "
+// boolean  => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof(age))
+console.log(typeof null);// object
+console.log(typeof(undefined))// undefined
