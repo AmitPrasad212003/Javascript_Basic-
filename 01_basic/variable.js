40b8e36abac1b3dab8b4558b8dc625bcfad39322
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-05-01 19:32:56 +0000

    learn variable in js

---
+++ 01_basic/variable.js
@@ -0,0 +1,22 @@
+console.log("Amit Prasad")
+
+const accountId = 123242
+
+let accountEmail = "Amit@gmail.home"
+
+var accountcity = "jaipur" 
+/*
+prefer not to use var 
+because of issue in block scope and functional scope.
+
+*/
+let accountState;
+
+console.log(accountId);
+// accountId  = 2 // not allowed
+
+accountEmail = "asdwf@gmail.home";
+accountPassord = "2312232r24";
+accountcity = "Ethelegfer";
+
+console.table([accountId,accountEmail,accountPassord,accountcity,accountState]);
