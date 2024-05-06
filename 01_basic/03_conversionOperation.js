c57f9b2c213a99238ef944ccce7f73386946c1a4
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2024-05-06 19:18:38 +0000

    pre&postfix

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -29,21 +29,72 @@
 //"amit" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
 
-// ************** Operation ***********//
+// // ************** Operation ***********//
 
-let value = 3
-let negValue = -value
-console.log(negValue);
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2/2);
-console.log(2**2);
-console.log(2%2);
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2/2);
+// // console.log(2**2);
+// // console.log(2%2);
+
+// let str1 = "HEllo"
+// let str2 = "Amit"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2+2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + 2);
+
+// console.log(true);
+// console.log(+true);
+// console.log("");
+// console.log(+"");
+
+let gameCounter = 100
+
+// gameCounter++;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
 
