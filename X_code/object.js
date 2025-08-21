888ea9b4b36c5e08f33d0429885e71cf43edea91
Amit Prasad <120964205+AmitPrasad212003@users.noreply.github.com>
2025-08-21 21:51:49 +0000

    object in js

---
+++ X_code/object.js
@@ -0,0 +1,12 @@
+const post = {
+    username : "@amitprasad",
+    content : "This is my #firstPost",
+    like : 150,
+    reposts : 5,
+    tags : ["@apnacollege", "@delta"]
+};
+
+console.log(post);
+console.log(post.content);
+console.log(post["content"]);
+
--- X_code/todo.js
+++ X_code/todo.js
@@ -24,6 +24,8 @@ while(true){
         let idx = prompt("please enter the task index.");
         todo.splice(idx, 1);
         console.log("Task Deleted");
+    }else{
+        console.log("Wrong request");
         
     }
 
@@ -31,3 +33,69 @@ while(true){
     req = prompt("please enter your request");
 
 }
+
+
+
+
+// practice 
+
+let arr = [1,2,3,4,5,6,2,3];
+let num = 2;
+for(let i = 0;i<arr.length;i++){
+    if(arr[i]==num){
+        arr.splice(i,1);
+    }
+
+}console.log(arr);
+
+
+// let number =  287152;
+// let count = 0;
+// let copy = number;
+
+// while(copy > 0){
+//     count++;
+//     copy = Math.floor(copy/10);
+// }
+
+// console.log(count);
+
+
+let number =  287152;
+let sum = 0;
+let copy = number;
+
+while(copy > 0){
+    digit = copy % 10;
+    sum += digit;
+    copy = Math.floor(copy/10);
+}
+
+console.log(`Sum of digits = ${sum}`);
+
+
+// factorial 
+
+let n = 5;
+let factorial = 1;
+
+for(let i = 1; i <= n; i++){
+    factorial *= i;
+}
+console.log(`Factorial of ${n} is ${factorial}`);
+
+
+
+// largest number in the array 
+
+let arr1 = [2,5,10,4,7,1,9];
+let largest = 0;
+
+for(let i = 0; i <= arr1.length; i++){
+    if(largest < arr1[i]){
+        largest = arr1[i];
+    }
+}
+
+console.log(`largest element of array is ${largest}`);
+
